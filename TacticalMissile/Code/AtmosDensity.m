20ce915d7aad804fc4efda12c670f0fcb925acca
Pauline <154602894+Philopolis@users.noreply.github.com>
2023-12-29 21:58:11 +0800

    Update AtmosDensity.m

--- TacticalMissile/Code/AtmosDensity.m
+++ TacticalMissile/Code/AtmosDensity.m
@@ -1,6 +1,8 @@
 %% 大气密度函数
 function rho = AtmosDensity(y)
-run('Constants.m');
+
+rho_0 = 1.2495;%标准海平面大气密度
+T_0 = 288.15;%标准海平面大气温度
 
 if y > 0
     rho = rho_0 .* ((T_0 - 0.0065 .* y)/T_0).^4.25588;
